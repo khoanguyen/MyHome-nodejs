14c27e1fc7ecf22c50c85feee036a30b3f0f1a01
Khoa Nguyen <minhkhoa4783@gmail.com>
2016-10-02 20:24:29 -0700

    Refine structure and packages

--- app.js
+++ app.js
@@ -4,8 +4,11 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+
+// routes
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var admin = require('./routes/admin-page');
 
 var app = express();
 
@@ -21,8 +24,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// routes config
 app.use('/', routes);
 app.use('/users', users);
+app.use('/admin', admin);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,16 +1,3 @@
 ﻿jQuery(function () {
-    //alert("ok");
-    c3r.layout.setupHandlers(jQuery);
-
-    jQuery('.nav-menu-item').click(function (evt) {
-        var target = jQuery(evt.target);
-        var pageId = target.data('target-page');
-        var page = jQuery(`#${pageId}`);
-       
-        jQuery('html, body').animate({
-            scrollTop: page.offset().top
-        }, 1000);
-
-        evt.preventDefault();
-    });    
+  
 });
